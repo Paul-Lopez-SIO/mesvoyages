814a421fd53c15c654fbb44d58b92d3027b545a1
Paulygone 3D <78047955+Paulygone3D@users.noreply.github.com>
2025-09-09 11:07:04 +0200

    Phase 4 : plusieurs pages

--- src/Controller/VoyagesController.php
+++ src/Controller/VoyagesController.php
@@ -1,14 +1,15 @@
 <?php
+
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class AccueilController extends AbstractController {
+class VoyagesController extends AbstractController {
     
-    #[Route('/', name: 'accueil')]
+    #[Route('/voyages', name: 'voyages')]
     public function index(): Response{
-        return $this->render("pages/accueil.html.twig");
+        return $this->render("pages/voyages.html.twig");
     }
 }
