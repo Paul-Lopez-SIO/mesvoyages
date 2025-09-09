e2c464436ddf5f73e710dde63b1584bc8fc02778
Paulygone 3D <78047955+Paulygone3D@users.noreply.github.com>
2025-09-09 10:45:57 +0200

    Phase 3 : utilisation de Twig

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -9,10 +10,10 @@ use Symfony\Component\Routing\Annotation\Route;
  *
  * @author pauly
  */
-class AccueilController {
+class AccueilController extends AbstractController {
     
     #[Route('/', name: 'accueil')]
     public function index(): Response{
-        return new Response('Coucou !');
+        return $this->render("pages/accueil.html.twig");
     }
 }
