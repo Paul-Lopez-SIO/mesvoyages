f9b8c9b3b70d58b7f31f2a2d99a3efd2386587b3
Paulygone 3D <78047955+Paulygone3D@users.noreply.github.com>
2025-09-15 15:21:20 +0200

    Phase 11 : ORM (Suppression d'un enregistrement)

--- src/Repository/VisiteRepository.php
+++ src/Repository/VisiteRepository.php
@@ -75,4 +75,8 @@ class VisiteRepository extends ServiceEntityRepository
                     ->getResult();
         }
     }
+    public function remove(Visite $visite): void{
+        $this->getEntityManager()->remove($visite);
+        $this->getEntityManager()->flush();
+    }
 }
