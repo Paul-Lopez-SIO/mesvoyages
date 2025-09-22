a3a168d37712a973263dadbcb448103deb2577c4
Paulygone 3D <78047955+Paulygone3D@users.noreply.github.com>
2025-09-22 14:56:28 +0200

    Phase 20 : Sécurité

--- src/Controller/VoyagesController.php
+++ src/Controller/VoyagesController.php
@@ -35,11 +35,13 @@ class VoyagesController extends AbstractController {
     }
     #[Route('/voyages/recherche/{champ}', name: 'voyages.findallequal')]
     public function findAllEqual($champ, Request $request): Response{
-        $valeur = $request->get("recherche");
-        $visites = $this->repository->findByEqualValue($champ, $valeur);
-        return $this->render("pages/voyages.html.twig", [
-            'visites' => $visites
-        ]);
+        if($this->isCsrfTokenValid('filtre_'.$champ, $request->get('_token'))){
+            $valeur = $request->get("recherche");
+            $visites = $this->repository->findByEqualValue($champ, $valeur);
+            return $this->render("pages/voyages.html.twig", [
+                'visites' => $visites
+            ]);
+        }
     }
     #[Route('voyages/voyage/{id}', name:'voyages.showone')]
     public function showOne($id): Response{
